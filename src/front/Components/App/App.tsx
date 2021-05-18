f7062729de1db84197b6d608f53295c2d7074576
Jan White <jw.coding@yandex.ru>
2021-05-19 01:03:55 +0300

    Add the main app component with switch to render routes

--- src/front/Components/App/App.tsx
+++ src/front/Components/App/App.tsx
@@ -1,8 +1,40 @@
-/** The app (duh) */
 import {hot} from 'react-hot-loader/root';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import {Home} from 'Components/Home/Home';
+import {Header} from 'Components/Header/Header';
+import {Board} from 'Components/Board/Board';
+import {Thread} from 'Components/Thread/Thread';
+import {Footer} from 'Components/Footer/Footer';
+
+import './Styles/globalStyles.scss';
+
+/**The App (duh) */
 let App = (): JSX.Element => (
-  <span>App will be here</span>
+  <Router>
+    <Switch>
+      <Route path='/' exact>
+        <Home/>
+      </Route>
+      <Route>
+        <Header/>
+        <Switch>
+          <Route path='/:board/:thread'>
+            <Thread/>
+          </Route>
+          <Route path='/:board'>
+            <Board/>
+          </Route>
+        </Switch>
+      </Route>
+    </Switch>
+    <Footer/>
+  </Router>
 );
 
 if (IN_DEV) {
